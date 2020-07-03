cffaadfa884230ba4539def337ad24815a605cfe
Dennis Bilson Jr <41457025+Quabynah-Codelabs@users.noreply.github.com>
2020-07-03 23:23:59 +0000

    Updated thhemes and styles
    Used SCSS

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+const ham =  document.querySelector('#hamb')
+const { body } = document
+
+ham.addEventListener('click', e =>{ 
+    body.classList.toggle('show-nav')
+})
--- script.js
+++
@@ -1,6 +0,0 @@
-const ham =  document.querySelector('#hamb')
-const { body } = document
-
-ham.addEventListener('click', e =>{ 
-    body.classList.toggle('show-nav')
-})
